28c1042300070bf8a8725fad27c04d75146d7208
SebasOso <criptodesc09@gmail.com>
2023-05-07 14:11:05 -0500

    StackHeapFinished

--- SectionFive/StackHeap.cpp
+++ SectionFive/StackHeap.cpp
@@ -6,16 +6,41 @@ class Character
 {
 	public:
 	Character();
+	~Character();
+	void SetName(string name);
+	string GetName();
+	void PrintCharacter();
 	private:
 	string name;
+	float health;
+};
+Character::~Character()
+{
+	cout<<GetName()<<" have been destroyed."<<endl;
+};
+void Character::PrintCharacter()
+{
+	cout<<"My character name is: "<<GetName()<<"."<<endl; 
+	cout<<"My character health is: "<<health<<"."<<endl;
+}
+void Character::SetName(string name)
+{
+	this->name = name;
+};
+string Character::GetName()
+{
+	return name;
 };
 Character::Character()
 {
 	cout<<"A new character has born!"<<endl;
+	health = 100.f;
 };
 int main(void)
 {
 	Character * myCharacter = new Character();
+	myCharacter->SetName("Sebas");
+	myCharacter->PrintCharacter();
 	Heap();
 	int* k = new int(5);
 	cout << "My int k ptr is: " << *k << endl;
@@ -24,6 +49,8 @@ int main(void)
 	cout << "My int k ptr is: " << *k << endl;
 	delete k;
 	k = nullptr;
+	delete myCharacter;
+	myCharacter = nullptr;
 	return 0;
 };
 void Heap()
