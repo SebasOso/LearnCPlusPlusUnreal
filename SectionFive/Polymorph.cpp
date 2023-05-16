7c7810a8e1c3cdb4ab92617fbe57290417ff247c
SebasOso <criptodesc09@gmail.com>
2023-05-16 16:28:02 -0500

    MultiInheritance

--- SectionFive/Polymorph.cpp
+++ SectionFive/Polymorph.cpp
@@ -16,24 +16,33 @@ class Dog:public Animal
     virtual void Noise() override; 
 };
 //_________________________________
-//Class Husky From Dog
-class Husky:public Dog
+//Class Wolf From Animal
+class Wolf:public Animal
 {
     public:
     virtual void Noise() override;
 };
 //_________________________________
+//Class Husky From Dog And Wolf
+class Husky: public Wolf, public Dog
+{
+    public:
+    virtual void Noise() override;
+};
 int main(void)
 {
     Animal *myAnimal = new Animal();
     Dog *myDog = new Dog();
+    Wolf *myWolf = new Wolf();
     Husky *myHusky = new Husky();
-
+    myHusky->Wolf::Noise();
+    myHusky->Dog::Noise();
+    myHusky->Noise();
     Animal *AnimalArray[] =
     {
         myAnimal,
         myDog,
-        myHusky
+        myWolf
     };
     for (size_t i = 0; i < 3; i++)
     {
@@ -48,11 +57,15 @@ void Animal::Noise()
 {
     cout<<"Animal Noise() called."<<endl;
 }
-void Husky::Noise()
+void Wolf::Noise()
 {
     cout<<"Hawwwwwwwwwwwwwwwww!"<<endl;
 };
 void Dog::Noise()
 {
     cout<<"Woof woof!"<<endl;
+};
+void Husky::Noise()
+{
+    cout<<"Owwww owww!"<<endl;
 };
